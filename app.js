ec5b957841c9c8cab941e55a7984d312a2e4ba3d
Bjorn Bennett <contact@bennettsdesign.com>
2019-02-23 14:06:10 -0600

    edit app.js

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-$(document).ready(function(){
+let doc = $(document),
+    kuc = $('#keyboard-upper-container'),
+    klc = $('#keyboard-lower-container');
+doc.ready(function(){
     $('#keyboard-upper-container').addClass('hide-me');
 
-    $(document).on('keydown', function(keyCode){
-        if( keyCode.which == '16' ){
+    doc.on('keydown', function(keyCode){
+        console.log('key: '+ keyCode.key + ', keycode: '+keyCode.which);
+        if( keyCode.key == 'Shift' ){
             console.log('shift key');
-            $('#keyboard-lower-container').addClass('hide-me');
-            $('#keyboard-upper-container').removeClass('hide-me');
+            klc.addClass('hide-me');
+            kuc.removeClass('hide-me');
         }
     }).on('keyup', function(){
-        console.log('key released');
-        $('#keyboard-upper-container').addClass('hide-me');
-        $('#keyboard-lower-container').removeClass('hide-me');
+        //console.log('shift key released');
+        kuc.addClass('hide-me');
+        klc.removeClass('hide-me');
     });
 
-    // $( "body" ).on( "keydown", function( event ) {
-    //     $( "#log" ).html( event.type + ": " +  event.which );
-    //   });
+    function keyPressed(a){
+        if( keyCode.which == a ){
+            console.log('key pressed');
+            $().addClass('highlighter');
+        }
+    }
+    function keyReleased(a){
+        console.log('key released');
+        $().removeClass('highlighter');
+    }
+
 });
