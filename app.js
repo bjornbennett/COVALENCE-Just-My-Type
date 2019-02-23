678fab41e73fb6981fab6be169abc83c306f3c57
Bjorn Bennett <contact@bennettsdesign.com>
2019-02-23 14:21:29 -0600

    highlight A keys is working

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 let doc = $(document),
     kuc = $('#keyboard-upper-container'),
-    klc = $('#keyboard-lower-container');
-doc.ready(function(){
-    $('#keyboard-upper-container').addClass('hide-me');
+    klc = $('#keyboard-lower-container'),
+    shiftDown;
 
+doc.ready(function(){
+    kuc.addClass('hide-me'); // hide uppercase keyboard
+
+    shiftDown = false;
     doc.on('keydown', function(keyCode){
         console.log('key: '+ keyCode.key + ', keycode: '+keyCode.which);
-        if( keyCode.key == 'Shift' ){
+
+        if( keyCode.key == 'Shift' ){ // if shift key, show/hide uppercase keyboard
             console.log('shift key');
             klc.addClass('hide-me');
             kuc.removeClass('hide-me');
+            shiftDown = true;
         }
-    }).on('keyup', function(){
-        //console.log('shift key released');
+        if(shiftDown == true ){
+            if( keyCode.key == 'A' ){
+                console.log('key "A" pressed');
+                $('#65').addClass('highlighter');
+            }
+        } else {
+            if( keyCode.key == 'a' ){
+                console.log('key a pressed');
+                $('#97').toggleClass('highlighter');
+            }
+        }
+    }).on('keyup', function(){ // release keys, hide uppercase keyboard, remove hightlighted letters class
+        console.log('shift key released');
         kuc.addClass('hide-me');
         klc.removeClass('hide-me');
+        $('.well').removeClass('highlighter');
+        shiftDown = false;
     });
 
-    function keyPressed(a){
-        if( keyCode.which == a ){
-            console.log('key pressed');
-            $().addClass('highlighter');
-        }
-    }
-    function keyReleased(a){
-        console.log('key released');
-        $().removeClass('highlighter');
-    }
+    // function keyPressed(keyCode, a){
+    //     if( keyCode.which == a ){
+    //         console.log('key pressed');
+    //         $().addClass('highlighter');
+    //     }
+    // }
+    // function keyReleased(a){
+    //     console.log('key released');
+    //     $().removeClass('highlighter');
+    // }
 
 });
